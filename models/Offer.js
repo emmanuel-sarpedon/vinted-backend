77a7a7124108b9964e534a42a1cc663f4db2c62c
Emmanuel SARPEDON <emmanuel.sarpedon@gmail.com>
2021-05-19 16:09:38 +0400

    edit Offer model - new limitations for name, description and max price

--- models/Offer.js
+++ models/Offer.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const Offer = mongoose.model("Offer", {
-  product_name: String,
-  product_description: String,
-  product_price: Number,
+  product_name: { type: String, maxlength: 50 },
+  product_description: { type: String, maxlength: 500 },
+  product_price: { type: Number, max: 100000 },
   product_details: Array,
   product_image: { type: mongoose.Schema.Types.Mixed, default: {} },
   owner: {
